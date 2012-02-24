11dcc78c06e1a0cf7b7bc6b15d9fa69c8c1a8f7a
Sharad Vikram <sharad.vikram@gmail.com>
2012-02-24 08:52:36 +0000

    wrote dao

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var Bridge = require('../../mqb-prototype/js/bridge/lib/bridge.js').Bridge;
-var bridge = new Bridge({host:'localhost'});
-console.log(bridge);
+var bridge = new Bridge({host:'localhost',port:8090,apiKey:"abcdefgh"});
+var Dao = require('./dao.js').Dao;
+var dao = new Dao('localhost','root','narsiodeyar1','quizbowl');
 var app = express.createServer();
 app.use(express.static(__dirname+'../public/static'));
+app.enable('jsonp callback');
 app.set('view engine', 'ejs');
-app.set('views','../views');
+app.set('views',__dirname+'../public/views');
 app.listen(1337);
+bridge.ready(function(){
+    console.log("Connected to Bridge");
+    bridge.publishService("dao",dao);
+    bridge.getService("dao", function(dao) { 
+      dao.search({answer:"dickens"},function(results){
+        //console.log(results)
+        }
+        )});
+    });
+app.get('/api/search', function (req,res){
+    dao.search(req.query, function(result){
+      res.json(result);
+      });
+    });
+app.get('/',function(req,res){
+    res.render('home');
+    });
