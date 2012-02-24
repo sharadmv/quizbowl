d920a2b3a1152adf200dbd98d3194ecd97bcceb7
Sharad Vikram <sharad.vikram@gmail.com>
2012-02-24 09:18:15 +0000

    dao through bridge

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ var bridge = new Bridge({host:'localhost',port:8090,apiKey:"abcdefgh"});
 var Dao = require('./dao.js').Dao;
 var dao = new Dao('localhost','root','narsiodeyar1','quizbowl');
 var app = express.createServer();
-app.use(express.static(__dirname+'../public/static'));
+app.use(express.static('../public/static/'));
 app.enable('jsonp callback');
 app.set('view engine', 'ejs');
-app.set('views',__dirname+'../public/views');
+app.set('views','../public/views/');
 app.listen(1337);
 bridge.ready(function(){
     console.log("Connected to Bridge");
