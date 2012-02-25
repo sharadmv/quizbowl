60d656bd06b4add2feddfc5a1bb3e74561540324
Sharad Vikram <sharad.vikram@gmail.com>
2012-02-25 05:10:08 +0000

    added models

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var Model = require('./model.js');
+var Ticker = Model.Ticker;
 var Bridge = require('../bridge/lib/bridge.js').Bridge;
 var bridge = new Bridge({host:'50.19.22.175',port:8090,apiKey:"abcdefgh"});
 var Dao = require('./dao.js').Dao;
 var dao = new Dao('50,19,22,175','root','narsiodeyar1','quizbowl');
 var app = express.createServer();
+var ticker;
 app.use(express.static('../public/static/'));
 app.enable('jsonp callback');
 app.set('view engine', 'ejs');
@@ -11,12 +14,14 @@ app.set('views','../public/views/');
 app.listen(1337);
 bridge.ready(function(){
     console.log("Connected to Bridge");
+    ticker = {
+      push:function(ticker){
+        console.log("TICKER: "+ticker);
+      }
+    }
+    bridge.joinChannel("ticker", ticker, function(channel){console.log(channel); channel.push("SUP")});
     bridge.publishService("dao",dao);
     bridge.getService("dao", function(dao) { 
-      dao.search({answer:"dickens"},function(results){
-        //console.log(results)
-        }
-        )});
     });
 app.get('/api/search', function (req,res){
     dao.search(req.query, function(result){
