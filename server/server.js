a45f89eda4c45e1fcbb8af3491c8c0211254579b
Sharad Vikram <sharad.vikram@gmail.com>
2012-06-20 00:03:51 -0700

    added deployment modes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 var express = require('express');
-
 var app = express.createServer();
+
+var DEPLOY = {
+  test:1337,
+  beta:8080,
+  release:80
+}
 
 app.get('/', function (req, res){
 	res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(DEPLOY[process.argv[2]]);
