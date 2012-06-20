43e37d5a522f6b5a430201ccff85d66220dd928f
Sharad Vikram <sharad.vikram@gmail.com>
2012-06-19 23:58:57 -0700

    added model js file

---
+++ server/model.js
@@ -0,0 +1,61 @@
+var Model = function(bridge) {
+  this.bridge = bridge;
+
+  Model.Constants:{
+    Bridge:{
+      ROOM_CHANNEL_PREFIX:"quizbowl-room-channel-",
+      ROOM_SERVICE_PREFIX:"quizbowl-room-service-"
+    }
+  };
+  Model.Dao:{
+    User:function(id, name, fbId, email, created){
+      this.id = id;
+      this.name = name;
+      this.fbId = fbId;
+      this.email = email;
+      this.created = created;
+    }
+  };
+  Model.Multiplayer:{
+    Room:function(name, host, callback) {
+      var name = name;
+      var host = host;
+      var created = new Date();
+
+      var users = [];
+      var channel;
+      var channelName = Model.Constants.Bridge.ROOM_CHANNEL_PREFIX+name;
+      var serviceName = Model.Constants.Bridge.ROOM_SERVICE_PREFIX+name;
+
+      Room.Handler = function(chat){
+        this.chat = function(user, message) {
+          chat(user, message);
+        };
+      }
+
+      this.join = function(user, handler, callback) {
+        users.push(user);
+        bridge.joinChannel(
+          channelName,
+          handler,
+          function(c, cn){
+            channel = c;
+            callback(channel);
+          }
+        );
+      }
+      this.getName = function(){
+        return name;
+      }
+      this.getHost = function(){
+        return host;
+      }
+      this.getCreated = function(){
+        return created;
+      }
+
+      bridge.publishService(serviceName, this, callback);
+    }
+  };
+}
+module.exports = Model;
