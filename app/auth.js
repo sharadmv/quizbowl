73e47e72a349dfb05df78c1cd5d68beaefc8f3f0
Achal <achalddave@live.com>
2012-06-25 22:57:07 -0700

    auth.js first implementation

--- app/auth.js
+++ app/auth.js
@@ -1,13 +1,26 @@
 var init = function(app) {
-  var auth = {
-    handler: {
-      login:function(userToken, callback){
-        //do fb query here to get userId; then query DAO for user object
-        app.getUsers()[userToken]=user;
-        callback(user);
-      }
-    }
-  }
-  return auth;
+	var auth = {
+		handler: {
+			login:function(userToken, callback){
+				// do fb query here to get userId; then query DAO for user object
+
+				fb.me(userToken, function(fbObject) {
+					var fBId = // fbObject something;
+					var user = dao.user.getFromFb(fbId, function(user) {
+						if (user === null) {
+							// id, name, fbId, email, created
+							user = new model.Dao.User(null, name, fbId, email, null);
+							dao.user.save(user);
+						}
+						app.getUsers()[userToken]=user;
+						callback(user);
+					});
+				});
+
+				function 
+			}
+		}
+	}
+	return auth;
 }
 module.exports = init;
