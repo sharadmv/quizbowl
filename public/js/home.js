c8b6d42ffb4b5f525c52db45e1d062c388afe558
Sharad Vikram <sharad.vikram@gmail.com>
2012-08-05 19:29:51 -0700

    basic splash page

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,26 @@
 (function() {
+  var scope = this;
+  var BASE_URL = "/api";
+  
+  var users;
+  var multi;
+  $.ajax({
+    url : BASE_URL+"/service?method=user.list",
+  }).done(function(response) {
+    users = response.data;
+    console.log(users);
+  });
 
+  bridge.ready(function() {
+    bridge.getService("quizbowl-"+namespace+"-multiplayer", function(m) {
+      multi = m;
+
+      multi.on("user_login", function(ev) {
+        users[ev.message.message.id] = ev.message.message;
+      });
+      multi.on("user_logout",function(ev) {
+        delete users[ev.message.message.id];
+      });
+    });
+  });
 })();
